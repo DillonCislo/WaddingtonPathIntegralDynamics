7c11081afd45a333185d17e55cf7e962ae9f0b6d
Dillon Cislo <djcislo@siggia-lab-workstation.rockefeller.edu>
2024-03-27 13:43:46 -0400

    Added potential kharmonic interpolation

---
+++ Source/evolveProbabilities.m
@@ -0,0 +1,118 @@
+function [allProb, viewTimes] = evolveProbabilities(initProb, T, ...
+    numTimeSteps, numViewTimes, dt, strictNormalization, useGPU)
+%EVOLVEPROBABILITIES Time evolve an initial probability distribution using
+%the (left Markov) transition probability operator
+%
+%   INPUT PARAMETERS:
+%
+%       - initProb:             #N x 1 initial probability distribution
+%
+%       - T:                    #N x #N transition probability matrix
+%
+%       - numTimeSteps:         The number of time steps over which to
+%                               evolve the initial probability
+%
+%       - numViewTimes:         The number of intermediate probability
+%                               distributions to record in the output
+%
+%       - dt:                   The time step accrued for each application
+%                               of the transition probability matrix. 
+%
+%       - strictNormalization:  Whether or not to re-normalize the
+%                               probability at every step to avoid problems
+%                               with roundoff error
+%
+%       - useGPU:               Whether or not to use the GPU to accelerate
+%                               time evolution
+%
+%   OUTPUT PARAMETERS:
+%
+%       - allProb:      #N x numViewTimes matrix of time-evolved
+%                       probability distributions
+%
+%       - viewTimes     1 x numViewTimes vector of times corresponding to
+%                       the entries in 'allProb'
+%
+%   by Dillon Cislo 2024/03/27
+
+%--------------------------------------------------------------------------
+% INPUT PROCESSING
+%--------------------------------------------------------------------------
+validateattributes(initProb, {'numeric'}, {'vector', 'nonnegative', 'finite', ...
+    'real' , '<=', 1});
+normP0 = sum(initProb);
+if (abs(1-normP0) > 2*eps(1))
+    warning(['Input probability distribution was not properly ' ...
+        'normalized. Renormalizing... ']);
+    initProb = initProb ./ normP0;
+end
+if (size(initProb, 2) ~= 1), initProb = initProb.'; end
+numStates = numel(initProb);
+
+validateattributes(T, {'numeric'}, {'2d', 'finite', 'real', ...
+    'nonnegative', 'nrows', numStates, 'ncols', numStates});
+normT = sum(T,1);
+if any(abs(1-normT) > 2*eps(1))
+    warning(['Input transition matrix was not properly ' ...
+        'normalized. Renormalizing... ']);
+    T = T ./ normT;
+end
+
+validateattributes(numTimeSteps, {'numeric'}, {'scalar', 'integer', ...
+    'positive', 'finite', 'real'});
+
+if ((nargin < 4) || isempty(numViewTimes))
+    numViewTimes = min(numTimeSteps+1, 500);
+end
+validateattributes(numViewTimes, {'numeric'}, {'scalar', 'integer', ...
+    'positive', 'finite', 'real', '<=', numTimeSteps+1});
+
+if ((nargin < 5) || isempty(dt)), dt = 1; end
+validateattributes(dt, {'numerc'}, {'scalar', 'positive', ...
+    'finite', 'real'});
+
+if ((nargin < 6) || isempty(strictNormalization))
+    strictNormalization = true;
+end
+validateattributes(strictNormalization, {'logical'}, {'scalar'});
+
+if ((nargin < 7) || isempty(useGPU)), useGPU = true; end
+validateattributes(useGPU, {'logical'}, {'scalar'});
+if useGPU, try gpuDevice; catch, useGPU = false; end; end
+
+
+%--------------------------------------------------------------------------
+% TIME EVOLVE PROBABILITIES
+%--------------------------------------------------------------------------
+
+viewTIDx = round(linspace(1, numTimeSteps+1, numViewTimes));
+assert(isequal(viewCounts, unique(viewCounts)), ...
+    'View time assignment failed');
+
+curProb = initProb;
+viewTimes = zeros(1, numViewTimes);
+allProb = zeros(numStates, numViewTimes);
+if (numViewTimes > 1), allProb(:,1) = curProb; end
+
+if useGPU
+    T = gpuArray(T);
+    curProb = gpuArray(curProb);
+    allProb = gpuArray(allProb);
+end
+
+for i = 1:numTimeSteps
+
+    curProb = T * curProb;
+    if strictNormalization, curProb = curProb ./ sum(curProb); end
+
+    [isViewTime, viewID] = ismember((i+1), viewTIDx);
+    if isViewTime
+        viewTimes(viewID) = i * dt;
+        allProb(:, viewID) = curProb;
+    end
+
+end
+
+allProb = gather(allProb);
+
+end
---
+++ Source/interpolatePotentialKHarmonic.m
@@ -0,0 +1,267 @@
+function U = interpolatePotentialKHarmonic(X, interpVals, interpIDx, k, varargin)
+%INTERPOLATEPOTENTIAL Interpolate a smooth potential on a set of points by
+%minimizing a k-harmonic energy.
+%
+%   INPUT PARAMETERS:
+%
+%       - X:            #N x dim set of input points
+%
+%       - interpVals:   #IV x 1 vector of known values to interpolate.
+%                       These essentially become Dirichlet boundary
+%                       conditions during minimization
+%
+%       - interpIDx:    #IV x 1 vector of point IDs corresponding to the
+%                       values in 'interpVals'
+%
+%       - k:            The power of the Laplacian (k = 1 is harmonic, 
+%                       k = 2 is biharmonic, etc). Defaults to biharmonic.
+%
+%   OPTIONAL INPUT PARAMETERS (Name, Value)-Pairs:
+%
+%       - ('TikhonovRegularization', regSigma = 1e-12): A small positive
+%       quantity used to make the quadratic problem positive definite.
+%
+%       - ('RemoveOutliers', removeOutliers = true): Whether or not to
+%       remove outliers generated during the interpolation process.
+%
+%       - ('OutlierThreshold', outlierThreshold = []): The threshold
+%       above and below which outliers are removed. If empty and
+%       removeOutliers == true, this is automatically set from the values
+%       of interpVals
+%
+%       - ('OutlierNeighbors', outlierNNSize = 10): The number of neighbors
+%       over which to average to in order to find the new value for any
+%       removed outliers.
+%
+%       - ('Laplacian', L = []): #N x #N point cloud Laplace-Beltrami
+%       operator
+%
+%       - ('MassMatrix', M = []): #N x #N diagonal mass matrix
+%       corresponding to L
+%
+%       - ('NormalizeMassMatrix', normalizeMassMatrix = true): Whether or
+%       not to normalize the mass matrix by its largest value (see
+%       'kharmonic.m')
+%
+%       - ('TimeStep', dt = -1): The time step used to construct the
+%       Laplace-Beltrami operator and mass matrix on the point cloud if the
+%       operators are not supplied by the user
+%
+%       - ('Verbose', verbose = false): Whether or not to report verbose
+%       progress output
+%
+%   OUTPUT PARAMETERS:
+%
+%       - U:            #N x 1 interpolated potential
+%
+%   by Dillon Cislo 2024/03/27
+
+%--------------------------------------------------------------------------
+% INPUT PROCESSING
+%--------------------------------------------------------------------------
+validateattributes(X, {'numeric'}, {'2d', 'finite', 'real'});
+numPoints = size(X,1); % dim = size(X,2);
+
+validateattributes(interpVals, {'numeric'}, {'vector', 'finite', 'real'});
+assert(numel(interpVals) < numPoints, ['Number of interpolation ' ...
+    'values exceeds the number of points']);
+if (size(interpVals, 2) ~= 1), interpVals = interpVals.'; end
+
+validateattributes(interpIDx, {'numeric'}, {'vector', 'integer', ...
+    'positive', 'finite', 'real', '<=', numPoints});
+if (size(interpIDx, 2) ~= 1), interpIDx = interpIDx.'; end
+assert(isequal(size(interpIDx), size(interpVals)), ...
+    'Interpolation input is improperly sized');
+assert(isequal(sort(interpIDx), unique(interpIDx)), ...
+    'Interpolation indices contain duplicate values');
+
+% Handle the trivial case of uniform values
+if (numel(unique(interpVals)) == 1)
+    U = unique(interpVals) .* ones(numPoints, 1);
+    return;
+end
+
+if (nargin < 4), k = 2; end
+if (isempty(k) || (k <= 0)), k = 2; end
+validateattributes(k, {'numeric'}, {'scalar', 'positive', ...
+    'integer', 'finite', 'real'});
+
+% OPTIONAL INPUT PROCESSING -----------------------------------------------
+
+regSigma = 1e-12;
+removeOutliers = true;
+outlierThreshold = [];
+outlierNNSize = 10;
+L = [];
+M = [];
+normalizeMassMatrix = true;
+dt = -1;
+verbose = false;
+
+for i = 1:numel(varargin)
+
+    if isa(varargin{i}, 'double'), continue; end
+    if isa(varargin{i}, 'logical'), continue; end
+
+    if strcmpi(varargin{i}, 'TikhonovRegularization')
+        regSigma = varargin{i+1};
+        validateattributes(regSigma, {'numeric'}, ...
+            {'scalar', 'nonnegative', 'finite', 'real'});
+    end
+
+    if strcmpi(varargin{i}, 'RemoveOutliers')
+        removeOutliers = varargin{i+1};
+        validateattributes(removeOutliers, {'logical'}, {'scalar'});
+    end
+
+    if strcmpi(varargin{i}, 'OutlierThreshold')
+        outlierThreshold = varargin{i+1};
+        if ~isempty(outlierThrehsold)
+            validateattribtues(outlierThreshold, {'numeric'}, ...
+                {'vector', 'numel', 2, 'finite', 'real'});
+            assert(outlierThreshold(2) > outlierThreshold(1), ...
+                ['Outlier threshold must have a second element ' ...
+                'that is greater than its first element']); 
+        end
+    end
+
+    if strcmpi(varargin{i}, 'OutlierNeighbors')
+        outlierNNSize = varargin{i+1};
+        validateattributes(outlierNNSize, {'numeric'}, ...
+            {'positive', 'integer', 'scalar', 'finite', 'real'});
+    end
+
+    if strcmpi(varargin{i}, 'Laplacian'), L = varargin{i+1}; end
+
+    if strcmpi(varargin{i}, 'MassMatrix'), M = varargin{i+1}; end
+
+    if strcmpi(varargin{i}, 'NormalizeMassMatrix')
+        normalizeMassMatrix = varargin{i+1};
+        validateattributes(normalizeMassMatrix, {'logical'}, {'scalar'});
+    end
+
+    if strcmpi(varargin{i}, 'TimeStep')
+        dt = varargin{i+1};
+        validateattributes(dt, {'numeric'}, {'scalar', 'finite', 'real'});
+        if (dt <= 0), dt = -1; end
+    end
+
+    if strcmpi(varargin{i}, 'Verbose')
+        verbose = varargin{i+1};
+        validateattributes(verbose, {'logical'}, {'scalar'});
+    end
+
+end
+
+if isempty(outlierThreshold)
+    outlierThreshold = [min(interpVals), max(interpVals)] + 1e-14 * [-1 1];
+end
+
+if (isempty(L) || isempty(M))
+
+    if verbose, fprintf('Building Laplacian/mass matrix... '); end
+    [L, M] = diffusionMapLaplacian(X, dt);
+    if verbose, fprintf('Done\n'); end
+
+else
+
+    validateattributes(L, {'numeric'}, {'2d', 'finite', 'real', ...
+        'ncols', numPoints, 'nrows', numPoints});
+    assert(issymmetric(L), 'Laplacian is not symmetric');
+
+    % Check for positive-definiteness
+    [~, isPD] = chol(L); isPD = isPD == 0;
+    if ~isPD
+        [~, isPD] = chol(-L); isPD = isPD == 0;
+        if isPD
+            L = -L;
+        else
+            error('Laplacian is neither positive nor negative definite');
+        end
+    end
+
+    validateattributes(M, {'numeric'}, {'2d', 'finite', 'real', ...
+        'ncols', numPoints, 'nrows', numPoints});
+    assert(isdiag(M), 'Mass matrix is not diagonal');
+    assert(all(diag(M) > 0), 'Mass matrix contains non-positive masses');
+
+end
+
+if normalizeMassMatrix, M = M ./ max(abs(diag(M))); end
+
+%--------------------------------------------------------------------------
+% INTERPOLATE POTENTIAL
+%--------------------------------------------------------------------------
+
+if verbose, fprintf('Computing interpolated potential... '); end
+
+% Build the k-laplacian, quadratic coefficients. NOTE: We have already
+% checked to ensure that L is positive definite
+switch k
+
+    case 1
+        Q = L;
+
+    case 2
+        Q = L*(M\L);
+
+    case 3
+        Q = L*((M\L)*(M\L));
+        Q = 0.5*(Q+Q');
+
+    otherwise
+        Q = L;
+        for ii = 2:k
+            Q = Q*(M\L);
+        end
+end
+
+if (regSigma > 0), Q = Q + regSigma .* eye(size(Q)); end
+
+U = min_quad_with_fixed(Q, zeros(numPoints, 1), interpIDx, interpVals);
+
+if verbose, fprintf('Done\n'); end
+
+%--------------------------------------------------------------------------
+% REMOVING OUTLIERS
+%--------------------------------------------------------------------------
+
+if removeOutliers
+
+    if verbose
+        fprintf('Handling interpolated potential outliers... ');
+    end
+
+    rmIDx = find( (U < outlierThreshold(1)) | ...
+        (outlierThreshold(2) < U) );
+
+    [nnIDx, nnDists] = knnsearch(X, X, 'K', numPoints);
+    nnIDx(:, 1) = []; nnDists(:, 1) = [];
+    assert(all(nnDists(:) > 0), 'Input point set contains duplicates');
+
+    for i = 1:numel(rmIDx)
+
+        curID = rmIDx(i); % The current outlier
+
+        % Determine the averaging neighborhood
+        curNNIDx = ~ismember(nnIDx(curID, :), rmIDx);
+        assert(sum(curNNIDx) >= outlierNNSize, ...
+            'Too few neighbors for outlier removal');
+        curNNIDx = find(curNNIDx, outlierNNSize, 'first');
+        curNNDists = nnDists(curID, curNNIDx);
+        curNNIDx = nnIDx(curID, curNNIDx);
+
+        % Averaging weights are proportional to inverse distance to the
+        % outlier
+        rmWeights = (1./curNNDists) ./ sum(1./curNNDists);
+
+        U(curID) = sum(rmWeights .* U(curNNIDx) .');
+
+    end
+
+    if verbose, fprintf('Done\n'); end
+
+end
+
+end
+
