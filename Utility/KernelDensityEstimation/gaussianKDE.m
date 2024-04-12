21ebe80633e844992a1db2cfa4fd8d66b4f34375
DillonCislo <dilloncislo@gmail.com>
2024-04-12 00:45:31 -0400

    modified gaussianKDE to just take a distance matrix

--- Utility/KernelDensityEstimation/gaussianKDE.m
+++ Utility/KernelDensityEstimation/gaussianKDE.m
@@ -1,4 +1,5 @@
-function pointDensity = gaussianKDE(P, Q, covK, bwK, verbose, weights)
+function pointDensity = ...
+    gaussianKDE(P, Q, covK, bwK, verbose, weights, distMatrix, useGPU)
 %GAUSSIANKDE Simple kernel density estimation for an input set of points
 %using a Gaussian kernel with user specified parameters:
 %
@@ -29,6 +30,14 @@ function pointDensity = gaussianKDE(P, Q, covK, bwK, verbose, weights)
 %       - weights: 1x#NP set of weights for each input point. Values in
 %                  'weights' must sum to one
 %
+%       - distMatrix:   #NQ x #NP distance matrix.
+%                       distMatrix(i,j) is the distance between query point
+%                       i and data point j
+%
+%       - useGPU:   Whether or not to perform computation on the GPU. Only
+%                   relevant if a pre-computed distance matrix is supplied
+%                   and used
+%
 %   OUTPUT PARAMETERS:
 %
 %       - pointDensity:     #NQ x 1 vector of pointwise density estimates
@@ -46,12 +55,14 @@ if ((nargin < 5) || isempty(verbose)), verbose = false; end
 if ((nargin < 6) || isempty(weights))
     weights = ones(size(P,1), 1) ./ size(P,1);
 end
+if (nargin < 7), distMatrix = []; end
+if (nargin < 8), useGPU = true; end
 
-% Process input suppot point cloud
+% Process input data point cloud
 validateattributes(P, {'numeric'}, {'2d', 'finite', 'real'});
 numDataPoints = size(P,1); dim = size(P,2);
 
-% Process input suppot point cloud
+% Process input query point cloud
 validateattributes(Q, {'numeric'}, {'2d', 'finite', 'real'});
 numQueryPoints = size(Q,1);
 assert(size(Q,2) == dim, ['Dimensionality of query points must match ' ...
@@ -94,7 +105,7 @@ if isscalar(bwK)
 elseif isvector(bwK)
     
     assert(numel(bwK) == dim, ['Number of elements in the vector ' ...
-        'representing isotropic bandwidth must equal ' ...
+        'representing diagonal bandwidth must equal ' ...
         'the dimensionality of the input points']);
     bwK = diag(bwK);
     
@@ -115,63 +126,124 @@ assert(detBW ~= 0, 'Bandwidth matrix must be nonsingular');
 validateattributes(verbose, {'logical'}, {'scalar'});
 
 % Validate input weights
-validateattributes( weights, {'numeric'}, {'vector', 'nonnegative', ...
-    'finite', 'real', 'numel', numDataPoints} );
+validateattributes(weights, {'numeric'}, {'vector', 'nonnegative', ...
+    'finite', 'real', 'numel', numDataPoints});
 if (size(weights, 1) ~= 1), weights = weights.'; end
 weights = weights ./ sum(weights); % Normalize just in case
 
-% Estimate Densities at Query Points --------------------------------------
-
-pointDensity = nan(numQueryPoints, 1);
-
-try
+% Validate distance matrix
+useDistMatrix = false;
+if ~isempty(distMatrix)
     
-    % Set up a parallel data queue to handle real-time progres outout
-    parDQ = parallel.pool.DataQueue;
-    afterEach(parDQ, @updateParallelProgressBar);
-    updateParallelProgressBar(1, numQueryPoints);
+    isBWKIso = isdiag(bwK) && ...
+        (max(abs(diag(bwK)-mean(diag(bwK)))) < 1e-12);
+    isCovKIso = isdiag(covK) && ...
+        (max(abs(diag(covK)-mean(diag(covK)))) < 1e-12);
     
-    parfor i = 1:numQueryPoints
+    if isBWKIso && isCovKIso
         
-        % The separation vectors between the current query point and all data
-        % points ([dim, numDataPoints] set of column vectors)
-        dij = (repmat(Q(i,:), numDataPoints, 1) - P).';
+        validateattributes(distMatrix, {'numeric'}, {'2d', 'finite', ...
+            'real', 'nonnegative'});
+        if (size(distMatrix,1) ~= size(distMatrix,2))
+            if isequal(size(distMatrix), [numDataPoints, numQueryPoints])
+                distMatrix = distMatrix.';
+            end
+        end
+        assert(isequal(size(distMatrix), [numQueryPoints, numDataPoints]), ...
+            'Distance matrix is improperly sized');
         
-        % Multiply the separation vectors by the bandwidth
-        % (A new [dim, numDataPoints] set of column vectors)
-        % dij = invBW * dij;
-        dij = bwK \ dij;
+        useDistMatrix = true;
         
-        % Take the dot product of the (bandwidth scaled) separation vectors to
-        % find the argument of the exponential in the kernel
-        % pointDensity(i) = sum(weights .* exp(-dot(dij, invCov * dij, 1)/2));
-        pointDensity(i) = sum(weights .* exp(-dot(dij, (covK \ dij), 1)/2));
-
-        % if verbose, progressbar(i, numQueryPoints), end
-        if verbose, send(parDQ, []); end
+    else
+        
+        if verbose
+            warning(['Covariance and bandwidth matrices must be ' ...
+                'isotropic to use utilize a pre-computed distance matrix. ' ...
+                'Ignoring distance matrix and working directly from points']);
+        end
         
     end
     
-catch
-    
-    for i = 1:numQueryPoints
-        
-        progressbar(i, numQueryPoints);
-        
-        % The separation vectors between the current query point and all data
-        % points ([dim, numDataPoints] set of column vectors)
-        dij = (repmat(Q(i,:), numDataPoints, 1) - P).';
-        
-        % Multiply the separation vectors by the bandwidth
-        % (A new [dim, numDataPoints] set of column vectors)
-        % dij = invBW * dij;
-        dij = bwK \ dij;
-        
-        % Take the dot product of the (bandwidth scaled) separation vectors to
-        % find the argument of the exponential in the kernel
-        % pointDensity(i) = sum(weights .* exp(-dot(dij, invCov * dij, 1)/2));
-        pointDensity(i) = sum(weights .* exp(-dot(dij, (covK \ dij), 1)/2));
+end
 
+validateattributes(useGPU, {'logical'}, {'scalar'});
+if useGPU, try gpuDevice; catch, useGPU = false; end; end
+
+% Estimate Densities at Query Points --------------------------------------
+
+if useDistMatrix
+    
+    if verbose, fprintf('Computing density from distance matrix\n'); end
+    
+    h = mean(diag(bwK));
+    sigma = mean(diag(covK));
+    
+    if useGPU
+        distMatrix = gpuArray(distMatrix);
+        h = gpuArray(h);
+        sigma = gpuArray(sigma);
+        weights = gpuArray(weights);
+    end
+    
+    pointDensity = exp(-distMatrix.^2 ./ (2 * h.^2 * sigma));
+    clear distMatrix
+    pointDensity = sum(weights .* pointDensity, 2);
+    pointDensity = gather(pointDensity);
+    
+else
+    
+    pointDensity = nan(numQueryPoints, 1);
+    
+    try
+        
+        % Set up a parallel data queue to handle real-time progres outout
+        parDQ = parallel.pool.DataQueue;
+        afterEach(parDQ, @updateParallelProgressBar);
+        updateParallelProgressBar(1, numQueryPoints);
+        
+        parfor i = 1:numQueryPoints
+            
+            % The separation vectors between the current query point and
+            % all data points ([dim, numDataPoints] set of column vectors)
+            dij = (repmat(Q(i,:), numDataPoints, 1) - P).';
+            
+            % Multiply the separation vectors by the bandwidth
+            % (A new [dim, numDataPoints] set of column vectors)
+            % dij = invBW * dij;
+            dij = bwK \ dij;
+            
+            % Take the dot product of the (bandwidth scaled) separation
+            % vectors to find the argument of the exponential in the kernel
+            % pointDensity(i) = sum(weights .* exp(-dot(dij, invCov * dij, 1)/2));
+            pointDensity(i) = sum(weights .* exp(-dot(dij, (covK \ dij), 1)/2));
+            
+            % if verbose, progressbar(i, numQueryPoints), end
+            if verbose, send(parDQ, []); end
+            
+        end
+        
+    catch
+        
+        for i = 1:numQueryPoints
+            
+            progressbar(i, numQueryPoints);
+            
+            % The separation vectors between the current query point and
+            % all data points ([dim, numDataPoints] set of column vectors)
+            dij = (repmat(Q(i,:), numDataPoints, 1) - P).';
+            
+            % Multiply the separation vectors by the bandwidth
+            % (A new [dim, numDataPoints] set of column vectors)
+            % dij = invBW * dij;
+            dij = bwK \ dij;
+            
+            % Take the dot product of the (bandwidth scaled) separation
+            % vectors to find the argument of the exponential in the kernel
+            % pointDensity(i) = sum(weights .* exp(-dot(dij, invCov * dij, 1)/2));
+            pointDensity(i) = sum(weights .* exp(-dot(dij, (covK \ dij), 1)/2));
+            
+        end
+        
     end
     
 end
@@ -180,7 +252,6 @@ end
 pointDensity = pointDensity ./ (detBW * sqrt(detCov) * sqrt(2 * pi)^dim);
 
 end
-
 
 function updateParallelProgressBar(curIter, maxIter)
 % Helper function to update the progressbar in parallel loops
