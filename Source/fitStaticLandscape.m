030196f2627360160a3ff7003b67ec518c7ffba9
Dillon Cislo <dilloncislo@gmail.com>
2024-12-19 12:49:24 -0500

    Refactored error metric computation into a standalone function

--- Source/fitStaticLandscape.m
+++ Source/fitStaticLandscape.m
@@ -1039,8 +1039,8 @@ clear upperBounds lowerBounds
 %==========================================================================
 % RUN OPTIMIZATION
 %==========================================================================
-if strcmpi(simTimeHandling, 'constant')
 
+if strcmpi(simTimeHandling, 'constant')
     optFun = @simulateLandscapeDynamicsConstTimeScale;
 else
     optFun = @simulateLandscapeDynamics;
@@ -1161,52 +1161,8 @@ else
 
             optCount = optCount + 1;
 
-            if strcmpi(errorType, 'symKLD')
-
-                curOptErr = (dataProb{i}(:, ii) - optSimProb{i}) .* ...
-                    log(dataProb{i}(:, ii) ./ optSimProb{i});
-                curOptErr(isnan(curOptErr)) = 0;
-                curOptErr = sum(curOptErr, 1);
-
-            elseif strcmpi(errorType, 'simKLD')
-
-                curOptErr = optSimProb{i} .* ...
-                    log(optSimProb{i} ./ dataProb{i}(:, ii));
-                curOptErr(isnan(curOptErr)) = 0;
-                curOptErr = sum(curOptErr, 1);
-
-            elseif strcmpi(errorType, 'dataKLD')
-
-                curOptErr = dataProb{i}(:, ii) .* ...
-                    log(dataProb{i}(:, ii) ./ optSimProb{i});
-                curOptErr(isnan(curOptErr)) = 0;
-                curOptErr = sum(curOptErr, 1);
-
-            elseif strcmpi(errorType, 'MSE')
-
-                curOptErr = ...
-                    sum((dataProb{i}(:, ii)-optSimProb{i}).^2, 1) ./ ...
-                    sum(dataProb{i}(:, ii).^2, 1);
-
-            elseif strcmpi(errorType, 'geoSphere')
-
-                curOptErr = ...
-                    acos(sum(sqrt(dataProb{i}(:, ii) .* optSimProb{i}), 1));
-
-            elseif strcmpi(errorType, 'EMD')
-                
-                curOptErr = zeros(1, size(optSimProb{i}, 2));
-                for kk = 1:size(optSimProb{i}, 2)
-                    [~, optWasserDist, ~] = PPMMOMT(X, X, ...
-                        optSimProb{i}(:, kk), dataProb{i}(:, ii), 200);
-                    curOptErr(kk) = optWasserDist(end);
-                end
-
-            else
-
-                error('Invalid error type');
-
-            end
+            curOptErr = computeSimulationError( ...
+                dataProb{i}(:, ii), optSimProb{i}, errorType, X);
 
             if strcmpi(simTimeHandling, 'none')
 
@@ -1309,48 +1265,8 @@ if verbose, fprintf('Done\n'); end
 
                 count = count + 1; % Increment in the index
 
-                if strcmpi(errorType, 'symKLD')
-
-                    err = (curDataProb(:,j) - simProb) ...
-                        .* log(curDataProb(:,j) ./ simProb);
-                    err(isnan(err)) = 0;
-                    err = sum(err, 1);
-
-                elseif strcmpi(errorType, 'simKLD')
-
-                    err = simProb .* log(simProb ./ curDataProb(:,j));
-                    err(isnan(err)) = 0;
-                    err = sum(err, 1);
-
-                elseif strcmpi(errorType, 'dataKLD')
-    
-                    err = curDataProb(:,j) .* log(curDataProb(:,j) ./ simProb);
-                    err(isnan(err)) = 0;
-                    err = sum(err, 1);
-
-                elseif strcmpi(errorType, 'MSE')
-
-                    err = sum((curDataProb(:,j)-simProb).^2, 1) ./ ...
-                        sum(curDataProb(:,j).^2, 1);
-
-                elseif strcmpi(errorType, 'geoSphere')
-
-                    err = acos(sum(sqrt(curDataProb(:,j) .* simProb), 1));
-
-                elseif strcmpi(errorType, 'EMD')
-
-                    err = zeros(1, size(simProb, 2));
-                    for tt = 1:size(simProb, 2)
-                        [~, wasserDist, ~] = PPMOMT(X, X, ...
-                            simProb(:, tt), curDataProb(:, j), 100);
-                        err(tt) = wasserDist(end);
-                    end
-
-                else
-
-                    error('Invalid error type');
-
-                end
+                err = computeSimulationError( ...
+                    curDataProb(:,j), simProb, errorType, X );
 
                 if strcmpi(simTimeHandling, 'none')
                     
@@ -1445,5 +1361,96 @@ if verbose, fprintf('Done\n'); end
             'UseGPU', useGPU);
 
     end
+
+end
+
+function err = computeSimulationError(measProb, simProb, errorType, X)
+%COMPUTESIMULATIONERROR A helper function that computes the error between
+%the measured probability at a single experimental time point and a set of
+%simulated probabilities.
+%
+%   WARNING: THIS FUNCTION IS NOT INTENDED FOR EXTERNAL USE. NO INTERNAL
+%   QUALITY CHECKS ARE PERFORMED
+%
+%   INPUT PARAMETERS:
+%
+%       - measProb:     #N x 1 vector of measured probabilities
+%
+%       - simProb:      #N x #T matrix of simulated probabilities
+%
+%       - errorType:    Character vector indicating error type.
+%
+%           - 'symKLD': symmetric K-L divergence D_KL(M||S) + D_KL(S||M)
+%
+%           - 'simKLD': K-L divergence relative to the simulated
+%           probabilities D_KL(S||M). Heuristically, this divergence is
+%           mode seeking
+%
+%           - 'measKLD': K-L divergence relative to measured probabilities
+%           D_KL(M||S). Heuristically this divergence is mean seeking
+%
+%           - 'MSE': Normalized L-2 error || M - S ||^2 ./ || M ||^2
+%
+%           - 'geoSphere': Note that the element-wise square root of any
+%           N-dimensional discrete probability distribution lies on the
+%           N-sphere (since sum(sqrt(P).^2) == 1). Given that embedding,
+%           this error computes the geodesic distance along the sphere
+%           between two probability distributions
+%
+%           - 'EMD': A "fast" approximation of the earth movers distance,
+%           treating (X, P) as signatures (i.e. a set of spatial points
+%           with corresponding weights). In some conceptual sense, this is
+%           the best error (since it takes spatial distance into account),
+%           but in practice it is too slow to use.
+%
+%       - X:            #N x dim set of input points on which the dynamics
+%                       are defined. Only used if the error type is 'EMD'
+%
+%   OUTPUT PARAMETERS:
+%
+%       - err:          1 x #T set of error values for each simulated
+%                       probability distribution
+%
+%   by Dillon Cislo 2024/06/25
+
+if strcmpi(errorType, 'symKLD')
+    
+    err = (measProb - simProb) .* log2(measProb ./ simProb);
+    err(isnan(err)) = 0;
+    err = sum(err, 1);
+    
+elseif strcmpi(errorType, 'simKLD')
+    
+    err = simProb .* log2(simProb ./ measProb);
+    err(isnan(err)) = 0;
+    err = sum(err, 1);
+    
+elseif strcmpi(errorType, 'dataKLD')
+    
+    err = measProb .* log2(measProb ./ simProb);
+    err(isnan(err)) = 0;
+    err = sum(err, 1);
+    
+elseif strcmpi(errorType, 'MSE')
+    
+    err = sum((measProb-simProb).^2, 1) ./ sum(measProb.^2, 1);
+    
+elseif strcmpi(errorType, 'geoSphere')
+    
+    err = acos(sum(sqrt(measProb .* simProb), 1));
+    
+elseif strcmpi(errorType, 'EMD')
+    
+    err = zeros(1, size(simProb, 2));
+    for tt = 1:size(simProb, 2)
+        [~, wasserDist, ~] = PPMMOMT(X, X, simProb(:, tt), measProb, 100);
+        err(tt) = wasserDist(end);
+    end
+    
+else
+    
+    error('Invalid error type');
+    
+end
 
 end
